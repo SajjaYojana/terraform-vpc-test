8f2a83b8e5251233ca8cf80f6ce87ebf8279a857
sajjayojana <sajjayojana014@gmail.com>
2024-06-04 12:09:10 -0500

    Vpc-test

--- provider.tf
+++ provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "daws78s-remotestate"
+    bucket = "daws-remote"
     key    = "expense-vpc"
     region = "us-east-1"
-    dynamodb_table = "daws78s"
+    dynamodb_table = "daws-remote"
   }
 }
 
